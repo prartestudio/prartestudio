6d62e40f0f8db4913f249a6599561491315b31f0
kanazuko <eduardo.amata.cs@gmail.com>
2022-07-02 15:28:58 -0500

    vs
    vs

---
+++ PRArtEstudio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRArtEstudio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
